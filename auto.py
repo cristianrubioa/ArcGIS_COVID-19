8a134d52022cef2ed72d0053f4f9ae8d837fa4de
cristianrubioa <cristianrubioa@gmail.com>
2020-10-30 09:46:01 -0500

    v1.0.3

--- auto.py
+++ auto.py
@@ -3,7 +3,7 @@
 
 __author__ = "Harold F. Murcia | Sergio A. Balaguera | Cristian F. Rubio Aguiar."
 __copyright__ = "Copyright 2020, Sistema de Respuesta COVID-19, Tolima"
-__version__ = "0.1.0"
+__version__ = "1.0.3"
 __status__ = "Dev"
 
 
@@ -11,7 +11,9 @@ __status__ = "Dev"
 
 import numpy as np
 import pandas as pd
-import time, os, sys
+import time
+import os
+import sys
 
 import warnings
 warnings.filterwarnings('ignore')
@@ -45,73 +47,65 @@ class conditioning(object):
         self.adjust_format()
 
     def san_andres_adjust(self, correct_name):
-        self.df = self.df.replace("Archipiélago de San Andrés Providencia y Santa Catalina", correct_name)
+        self.df = self.df.replace(
+            "Archipiélago de San Andrés Providencia y Santa Catalina", correct_name)
 
     def Mariquita_adjust(self, correct_name):
         self.df = self.df.replace("San Sebastián de Mariquita", correct_name)
 
     def date_adjust(self):
-        self.df.fecha_de_notificaci_n = self.df.fecha_de_notificaci_n.str.replace("T00:00:00.000","")
-        self.df.fecha_diagnostico = self.df.fecha_diagnostico.str.replace("T00:00:00.000","")
-        self.df.fecha_diagnostico = self.df.fecha_diagnostico.str.replace("-   -","")
-        self.df.fecha_diagnostico = self.df.fecha_diagnostico.str.replace("SIN DATO","")
-        self.df.fecha_recuperado = self.df.fecha_recuperado.str.replace("T00:00:00.000","")
-        self.df.fecha_recuperado = self.df.fecha_recuperado.str.replace("-   -","")
-        self.df.fecha_reporte_web = self.df.fecha_reporte_web.str.replace("T00:00:00.000","")
-        self.df.fecha_reporte_web = self.df.fecha_reporte_web.str.replace("-   -","")
-        self.df.fecha_de_muerte = self.df.fecha_de_muerte.str.replace("T00:00:00.000","")
-        self.df.fecha_de_muerte = self.df.fecha_de_muerte.str.replace("-   -","")
-        self.df.fis = self.df.fis.str.replace("T00:00:00.000","")
-        self.df.fis = self.df.fis.str.replace("Asintomático","")
-        self.df.fecha_de_notificaci_n = pd.to_datetime(self.df['fecha_de_notificaci_n'])
-        self.df.fecha_de_notificaci_n = self.df.fecha_de_notificaci_n.dt.strftime('%m/%d/%Y')
-        self.df.fecha_diagnostico = pd.to_datetime(self.df['fecha_diagnostico'])
-        self.df.fecha_diagnostico = self.df.fecha_diagnostico.dt.strftime('%m/%d/%Y')
-        print(self.df.fecha_reporte_web)
-        self.df.fecha_reporte_web = pd.to_datetime(self.df['fecha_reporte_web'])
-        self.df.fecha_reporte_web = self.df.fecha_reporte_web.dt.strftime('%m/%d/%Y')
-        self.df.to_csv("data/Casos_positivos_de_COVID-19_en_Colombia_filtered.csv", index=False, encoding='utf-8-sig')
+        print(self.df.fecha_diagnostico)
+
+        self.df.to_csv("data/Casos_positivos_de_COVID-19_en_Colombia_filtered.csv",
+                       index=False, encoding='utf-8-sig')
 
         # Colombia
         Data_rank = self.df.groupby(['departamento']).count()
-        Data_rank = Data_rank[['id_de_caso','edad']]
+        Data_rank = Data_rank[['id_de_caso', 'edad']]
         Data_rank['id_de_caso'] = Data_rank.index
-        Data_rank.columns = ['departamento','Total casos']
-        Data_rank=Data_rank.sort_values(by=['Total casos'], ascending=False)
+        Data_rank.columns = ['departamento', 'Total casos']
+        Data_rank = Data_rank.sort_values(
+            by=['Total casos'], ascending=False)
         print(Data_rank)
-        Data_rank.to_csv("data/ranking_acumulado.csv", index=False, encoding='utf-8-sig')
+        Data_rank.to_csv("data/ranking_acumulado.csv",
+                         index=False, encoding='utf-8-sig')
 
         # Tolima
         self.df = self.df.sort_values(by=['fecha_reporte_web'])
         print(self.df.shape)
         Tolima_acum = self.df
-        drop_rows = Tolima_acum.loc[(Tolima_acum['departamento']!='Tolima')]
+        drop_rows = Tolima_acum.loc[(Tolima_acum['departamento'] != 'TOLIMA')]
         Tolima_acum["ID_Tolima"] = ""
         Tolima_acum = Tolima_acum.drop(drop_rows.index)
         N_cases = Tolima_acum.shape[0]
-        for k in range(0,N_cases):
+        for k in range(0, N_cases):
             i = Tolima_acum.index[k]
-            pos = Tolima_acum.index[Tolima_acum['id_de_caso'] == Tolima_acum['id_de_caso'][i] ]
+            pos = Tolima_acum.index[Tolima_acum['id_de_caso']
+                                    == Tolima_acum['id_de_caso'][i]]
             Tolima_acum['ID_Tolima'][pos] = int(k+1)
-        Tolima_acum = Tolima_acum[['fecha_diagnostico','ID_Tolima','ciudad_de_ubicaci_n']]
-        Tolima_acum.to_csv("data/Tolima.csv", index=False, encoding='utf-8-sig')
+        Tolima_acum = Tolima_acum[[
+            'fecha_diagnostico', 'ID_Tolima', 'ciudad_de_ubicaci_n']]
+        Tolima_acum.to_csv("data/Tolima.csv", index=False,
+                           encoding='utf-8-sig')
 
     def adjust_format(self):
-        tolima_data = pd.read_csv('data/Tolima.csv')            
+        tolima_data = pd.read_csv('data/Tolima.csv')
         tolima_data = tolima_data.fillna(method="ffill")
 
-        tolima_data['fecha_diagnostico']=pd.to_datetime(tolima_data['fecha_diagnostico'].astype(str), format='%m/%d/%Y')
-        tolima_data['fecha_diagnostico'] = tolima_data['fecha_diagnostico'].dt.strftime('%d-%m-%Y')
-
         tolima_data['Object_ID'] = tolima_data['ID_Tolima']
-        tolima_data = tolima_data.reindex(columns=['Object_ID','fecha_diagnostico', 'ID_Tolima', 'ciudad_de_ubicaci_n'])
-        tolima_data.to_csv('data/Tolima.csv', index=False, encoding='utf-8-sig')
+        tolima_data = tolima_data.reindex(
+            columns=['Object_ID', 'fecha_diagnostico', 'ID_Tolima', 'ciudad_de_ubicaci_n'])
+        tolima_data.to_csv('data/Tolima.csv', index=False,
+                           encoding='utf-8-sig')
 
 
 if __name__ == "__main__":
-    #os.system("bash -c '%s'" % bash_script)
+    os.system("bash -c '%s'" % bash_script)
     csv_path = os.getcwd()
-    csv_path = os.path.join(csv_path, 'data/Casos_positivos_de_COVID-19_en_Colombia.csv')
-    columnas = ['id_de_caso','fecha_de_notificaci_n','codigo_divipola','ciudad_de_ubicaci_n','departamento','atenci_n','edad','sexo','tipo','estado','pa_s_de_procedencia','fis','fecha_de_muerte','fecha_diagnostico','fecha_recuperado','fecha_reporte_web','tipo_recuperaci_n','cod_depto','cod_pais','Pertenencia_etnica','Nombre_grupo_etnico', 'nuevo']
+    csv_path = os.path.join(
+        csv_path, 'data/Casos_positivos_de_COVID-19_en_Colombia.csv')
+
+    columnas = ['fecha_reporte_web', 'id_de_caso', 'fecha_de_notificaci_n', 'cod_depto', 'departamento', 'codigo_divipola', 'ciudad_de_ubicaci_n', 'edad', 'uuu', 'sexo', 'tipo', 'ubicacion_caso', 'estado',
+                'cod_pais', 'pa_s_de_procedencia', 'atenci_n', 'fis', 'fecha_de_muerte', 'fecha_diagnostico', 'fecha_recuperado', 'tipo_recuperaci_n', 'pertenencia_etnica', 'nombre_grupo_etnico']
     data = pd.read_csv(csv_path, names=columnas)
     result = conditioning(data)
